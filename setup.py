3a1912ac49c65f8a54ddb9051cdf63176a819603
Yogesh Kasar <kasary778@gmail.com>
2025-12-28 16:30:31 +0530

    First set up

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPN_E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    '''this function will return the list of requiremtns'''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', "") for req in requirements]
+
+        if HYPN_E_DOT in requirements:
+            requirements.remove(HYPN_E_DOT)
+    return requirements
+
+setup(
+    name="ML_PROJECTS",
+    version="0.0.1",
+    author="Yogesh Kasar",
+    author_email="kyogesh757@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
